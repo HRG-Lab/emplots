2629354a9d482ad80399af4fad48325d2bf075d3
Ryan Campbell <rdcampbell1990@gmail.com>
2018-10-26 16:15:18 -0400

    Add documentation to rectangular functions

--- emplots/rectangular.py
+++ emplots/rectangular.py
@@ -4,8 +4,41 @@ import scipy as sp
 
 pi = np.pi
 
-def plot(axes=None, angles=None, magnitudes=None, csv_file=None):
-    """Takes in a number of different possible arguments and generates a plot.
+def plot(axes=None, angles=None, magnitudes=None, csv_file=None, **kwargs):
+    """
+    Takes in a number of different possible arguments and generates a plot.
+
+    The arguments can either be an array of angles, AND an array of magnitudes,
+    or you can pass a path to a csv file, which will then plot that data
+
+    Parameters
+    ----------
+    axes : matplotlib.axes.Axes, optional, default=None
+        The axis on which the plot will be generated. If None is passes, a new
+        axis will be generated
+
+    angles : array-like, optional, default: None
+        The angles over which there is data (The independent variable)
+
+    magnitudes : array-like, optional, default: None
+        The magnitudes of the radiation pattern
+
+    csv_file : string or path, optional, default: None
+        The path to a csv_file containing data to plot
+
+    Other Parameters
+    ----------------
+    **kwargs : `matplotlib.axes.Line2D` properties, optional
+        *kwargs* are those expected by matplotlib. In this way, you can specify
+        all the line properties you can pass to matplotlib's plot function.
+        For example if you pass `color='black'`, this will get passed to the
+        underlying matplotlib function and the line color will be black. Consult
+        matplotlibs documentation for more information.
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The axes with the newly generated plot.
     """
 
     if angles is None and magnitudes is None and csv_file is None:
@@ -22,17 +55,37 @@ def plot(axes=None, angles=None, magnitudes=None, csv_file=None):
     if csv_file:
         pass # TODO
     else:
-        ax = _plot_magnitude(axes, angles, magnitudes)
+        ax = _plot_magnitude(axes, angles, magnitudes, **kwargs)
 
     return ax
 
-def plot_min_mean_max(ax, angles=None, min=None, mean=None, max=None):
+def plot_min_mean_max(angles, min, mean, max, axes=None):
     """
     Plot the minimum, maximum, and average, shading the area between the max and min
-    """
-    if (min is None) or (mean is None) or (max is None):
-        raise ValueError("Must pass all three of min, mean, and max")
 
+    Parameters
+    ----------
+    angles : array-like
+        The angles over which there is data (The independent variable)
+
+    min : array-like
+        The lower-bounding values of the magnitudes of the radiation pattern
+
+    mean : array-like
+        The average values of the magnitudes of the radiation pattern
+
+    max : array-like
+        The upper-bounding values of the magnitudes of the radiation pattern
+
+    ax : matplotlib.axes.Axes
+        The axis on which the plot will be generated
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The axes with the newly generated plot. Returns the axes passed to the
+        function. If no axes was passed, a new axes is created and returned
+    """
     ref_array_len = len(angles)
     if all(len(lst) != ref_array_len for lst in [min, mean, max]):
         raise ValueError("All arrays must be the same length")
@@ -58,19 +111,55 @@ def plot_min_mean_max(ax, angles=None, min=None, mean=None, max=None):
     return ax
 
 def _plot_from_csv(csv_file):
-    """Takes a csv file and generates a radiation pattern
+    """
+    Takes a csv file and generates a radiation pattern
     """
     # TODO
     pass
 
-def _plot_magnitude(ax, angles=None, magnitudes=None, normalize=True, convertToDb=True, **kwargs):
-    """Takes two array like objects of equal dimension and generates a radiation pattern
-
-    This function takes raw magnitude values, normalizes them, then converts
-    to dB. It then plots them on a new subplot which is then returned from the
-    function.
+def _plot_magnitude(ax, angles, magnitudes, normalize=True, convertToDb=True, **kwargs):
     """
+    Takes two array like objects of equal dimension and generates a radiation pattern
 
+    An internal function meant to be called by rectangular.plot()
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axis on which the plot will be generated
+
+    angles : array-like
+        The angles over which there is data (The independent variable)
+
+    magnitudes : array-like
+        The magnitudes of the radiation pattern
+
+    normalize : bool, optional, default: True
+        This function expects to have to normalize the data passed to it. If the
+        data is already normalized, pass False, and the function will leave the
+        data as is.
+
+    convertToDb : bool, optional, default: True
+        In addition to expecting to normalize the data passed to it, this
+        function also expects to have to convert the magnitudes to dB (after
+        normalization). If this is already done, pass False, and the function
+        will leave the data as is.
+
+
+    Other Parameters
+    ----------------
+    **kwargs : `matplotlib.axes.Line2D` properties, optional
+        *kwargs* are those expected by matplotlib. In this way, you can specify
+        all the line properties you can pass to matplotlib's plot function.
+        For example if you pass `color='black'`, this will get passed to the
+        underlying matplotlib function and the line color will be black. Consult
+        matplotlibs documentation for more information.
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The axes with the newly generated plot.
+    """
     if len(angles) != len(magnitudes):
         raise ValueError("Array lengths must be equal")
 
